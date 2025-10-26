5fb5b2a44dca3ba32386e06e765ba6d863ef2a91
vogguabhiram <vogguabhiram2005@gmail.com>
2025-10-26 22:01:15 +0530

    Remove event listener for showing resume

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const btn = document.getElementById('showResumeBtn');
-  const intro = document.getElementById('intro');
-  const resume = document.getElementById('resume');
 
-  btn.addEventListener('click', function() {
-    intro.classList.add('hidden');       // hide button section
-    resume.classList.remove('hidden');   // show resume section
-  });
-});
 
